9a0e84dbe90e87d5ba47fc108123a981bb9d55c1
SukbeomH <brent93.dev@gmail.com>
2026-06-12 19:59:02 +0900

    chore: commit pending printer config changes

--- scripts/reset_crowsnest.sh
+++ scripts/reset_crowsnest.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -eu
 
-sudo -n /usr/bin/systemctl reset-failed crowsnest
-sudo -n /usr/bin/systemctl restart crowsnest
+curl -fsS \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{"service":"crowsnest"}' \
+  http://127.0.0.1:7125/machine/services/restart >/dev/null
+
 sleep 2
 /usr/bin/systemctl --no-pager --full is-active crowsnest
